2291c232c2e523d1769ca125c8caaad4ea99c962
Marcello Pogliani <pogliamarci@hotmail.it>
2017-02-22 13:45:14 +0100

    Other cosmetic changes

--- assets/js/home-page-type.js
+++ assets/js/home-page-type.js
@@ -2,6 +2,7 @@ $(function(){
     $(".type-is-coming").typed({
         strings: ["Fun", "A wonderful challenge", "<span class=\"logo-draw\">2017 \\> </span><span class=\"logo-poli\">poli</span><span class=\"logo-ctf\">ctf</span>"],
         typeSpeed: 0,
+        backDelay: 900,
         callback: function() {
             $( "span" ).siblings( ".typed-cursor" ).css( "animation", "none" );
             $( "span" ).siblings( ".typed-cursor" ).hide();
